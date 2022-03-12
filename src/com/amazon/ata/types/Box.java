f3ef05f15bf80926df35dd1f9ce4669c495e5208
Svetlana Baranova <svperlova@gmail.com>
2022-03-11 16:01:52 -0800

    Submition for CodeGrade

---
+++ src/com/amazon/ata/types/Box.java
@@ -0,0 +1,17 @@
+package com.amazon.ata.types;
+
+import java.math.BigDecimal;
+
+public class Box extends Packaging {
+    /**
+     * Instantiates a new Packaging object.
+     *
+     * @param material - the Material of the package
+     * @param length   - the length of the package
+     * @param width    - the width of the package
+     * @param height   - the height of the package
+     */
+    public Box(Material material, BigDecimal length, BigDecimal width, BigDecimal height) {
+        super(material, length, width, height);
+    }
+}
